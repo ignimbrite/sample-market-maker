1bdaa3861103cc3fb16d44a1bf00d644147e316d
hellboy <matiascfh98@gmail.com>
2023-07-06 12:35:27 -0300

    Offset and Refresh Time

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# woo-maker
-Sample WooX Market Making Bot
+woo-maker
+A simple WooX Market Making Bot for handling and managing cryptocurrency orders. This bot helps place bid and ask orders on the basis of pre-defined offset and refresh intervals.
+
+Requirements
+Python 3.7 or newer
+An account with WooX with API keys available
+Installation
+Clone this repository and navigate into the project directory, then install the necessary libraries:
+
+bash
+Copy code
+git clone https://github.com/username/woo-maker.git
+cd woo-maker
+pip install -r requirements.txt
+Replace username with your actual GitHub username.
+
+Usage
+To use the bot, you need to provide it with the offset and refresh time parameters as command line arguments when running the script. Offset is provided in basis points and refresh time in milliseconds.
+
+Run the bot with the following command:
+
+css
+Copy code
+python main.py --offset [offset_bps] --refresh [refresh_time_ms]
+For example, to run the bot with an offset of 3 basis points and a refresh time of 5000 milliseconds (or 5 seconds), you would use:
+
+css
+Copy code
+python main.py --offset 3 --refresh 5000
+Configuration
+You need to provide the bot with your API keys. Open the main.py file and replace the placeholders in the following lines with your actual API public key, API secret key, and application ID:
+
+python
+Copy code
+api_public_key='',
+api_secret_key='',
+application_id='',
+Be sure to keep your API keys secure and don't share them with anyone.
+
--- main.py
+++ main.py
@@ -1,28 +1,27 @@
+import argparse
 import asyncio
 import logging
 from woo_x import Client
 
 class MarketMaker:
-    def __init__(self, client, offset):
+    def __init__(self, client, offset_bps, refresh_time_ms):
         self.client = client
-        self.best_bid = None
-        self.best_ask = None
-        self.offset = offset
+        self.mid_price = None
+        self.offset_bps = offset_bps
+        self.refresh_time_ms = refresh_time_ms
         self.price_updated = asyncio.Event()
 
     async def update_best_prices(self, symbols):
         async for slam in self.client.slams(symbols):
-            if 'orders' in slam and 'bid' in slam['orders'] and 'price' in slam['orders']['bid']:
-                self.best_bid = slam['orders']['bid']['price']
-            if 'orders' in slam and 'ask' in slam['orders'] and 'price' in slam['orders']['ask']:
-                self.best_ask = slam['orders']['ask']['price']
+            if 'mid_price' in slam:
+                self.mid_price = slam['mid_price']
             self.price_updated.set()
 
     async def place_order(self, symbol, side, size):
         await self.price_updated.wait()
-        if self.best_bid is not None and self.best_ask is not None:
-            mid_price = (self.best_bid + self.best_ask) / 2
-            price = mid_price - self.offset if side == 'bids' else mid_price + self.offset
+        if self.mid_price is not None:
+            offset = self.mid_price * (self.offset_bps / 1e4)
+            price = round(self.mid_price - offset if side == 'bids' else self.mid_price + offset, 2)
             if price:
                 response = await self.client.place_order(symbol, side, price, size)
                 print(response)
@@ -39,7 +38,7 @@ class MarketMaker:
             await self.cancel_all_orders()
             await self.place_order('PERP_BTC_USDT', 'bids', 0.01)
             await self.place_order('PERP_BTC_USDT', 'asks', 0.01)
-            await asyncio.sleep(3)
+            await asyncio.sleep(self.refresh_time_ms / 1e3)
 
     async def run(self):
         symbols = ['PERP_BTC_USDT']
@@ -47,7 +46,7 @@ class MarketMaker:
         handle_orders_task = asyncio.create_task(self.handle_orders())
         await asyncio.gather(update_task, handle_orders_task)
 
-async def main():
+async def main(offset_bps, refresh_time_ms):
     logging.basicConfig(
         level=logging.ERROR
     )
@@ -58,9 +57,12 @@ async def main():
         application_id='194c85b6-8114-465c-829c-9fca81e40bf4',
         network='testnet',
     ) as client:
-        offset = 300
-        executor = MarketMaker(client, offset)
+        executor = MarketMaker(client, offset_bps, refresh_time_ms)
         await executor.run()
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    parser = argparse.ArgumentParser(description="Market Maker bot")
+    parser.add_argument('--offset', type=float, help='Offset in basis points', required=True)
+    parser.add_argument('--refresh_time', type=int, help='Refresh time in milliseconds', required=True)
+    args = parser.parse_args()
+    asyncio.run(main(args.offset, args.refresh_time))
